f1c259b895ed388d36bca487b51707e716f0287a
Sergey Korchinskiy <k.serjey@gmail.com>
2016-12-30 15:11:36 +1000

    [main][fix] Getting expression from args

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import argparse
 from src.utils import *
 
 parser = argparse.ArgumentParser(prog='boolean-calc', description='Calculator that build logical table from boolean expression')
-parser.add_argument('expression', nargs='+', help='boolean expression')
+parser.add_argument('expression', type=str, help='boolean expression')
 args = parser.parse_args()
 
+expression = args.expression
 print("Expression:", expression)
 
 format(parse_expression(expression))
